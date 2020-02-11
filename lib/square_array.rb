936d2a6c3a26de0992c54c2f2a5c65c56c8b5472
Ben Dunjay <bendunjay@outlook.com>
2020-02-11 14:40:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -13,7 +13,20 @@ def square_array(array)
  
 end
 
-
+def square_array(array)
+ counter = 0 
+ new_array = []
+ while counter < array.length do
+   new_array.push (array[counter**2])
+   counter +=1 
+  
+ end
+ return new_array
+ 
+ 
+ 
+ 
+end
 
 
 
